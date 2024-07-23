2ef92d94dbdcd981b2659245053b1c5ce297bd34
MafKamRAN <143469368+MafKamRAN@users.noreply.github.com>
2024-07-23 03:53:18 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("mousemove", effect);
+const layers = document.querySelectorAll(".layers");
+function effect(e) {
+  layers.forEach((layer) => {
+    const movement = layer.getAttribute("data-speed");
+    let x = (window.innerWidth - e.pageX * movement) / 250;
+    let y = (window.innerWidth - e.pageX * movement) / 250;
+    layer.style.transform = `translate(${-x}px) translateY(${y}px)`;
+  });
+}
